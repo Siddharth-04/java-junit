d5527728810ac3183018ad5586be27d76a99384b
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-04-02 14:14:40 +0530

    [Siddharth] ADD: String Utility Methods testing

---
+++ src/main/java/bridgelabz/basicJunitTest/StringUtils.java
@@ -0,0 +1,35 @@
+package bridgelabz.basicJunitTest;
+
+
+public class StringUtils {
+
+    public StringBuilder s;
+
+    public StringUtils(String str) {
+        this.s = new StringBuilder(str);
+    }
+
+    public String reverse(){
+        return this.s.reverse().toString();
+    }
+
+    public boolean isPalindrome(){
+        int i=0,j=s.length()-1;
+
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+//            else{
+                i++;
+                j--;
+//            }
+        }
+
+        return true;
+    }
+
+    public String toUpperCase(){
+        return this.s.toString().toUpperCase();
+    }
+}
---
+++ src/test/java/bridgelabz/StringUtilsTest.java
@@ -0,0 +1,31 @@
+package bridgelabz;
+
+import bridgelabz.basicJunitTest.StringUtils;
+import org.junit.jupiter.api.*;
+import static org.junit.Assert.*;
+
+public class StringUtilsTest {
+     static StringUtils stringTest;
+
+    @BeforeAll
+    public static void initString(){
+        stringTest = new StringUtils("racecar");
+        System.out.println(stringTest.s);
+    }
+
+    @Test
+    public void reverseTest(){
+        assertEquals("racecar",stringTest.reverse());
+    }
+
+    @Test
+    public void isPalindrome(){
+        assertEquals(true,stringTest.isPalindrome());
+    }
+
+    @Test
+    public void convertUpperCase(){
+        assertEquals("RACECAR",stringTest.toUpperCase());
+    }
+}
+//racecar
