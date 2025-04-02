0753703be25a328eb8377474fb03021225697399
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-04-02 11:40:45 +0530

    Testing calculator class

---
+++ src/main/java/bridgelabz/basicJunitTest/Calculator.java
@@ -0,0 +1,27 @@
+package bridgelabz.basicJunitTest;
+
+public class Calculator {
+    public int a;
+    public int b;
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ src/test/java/bridgelabz/CalculatorTest.java
@@ -0,0 +1,31 @@
+package bridgelabz;
+
+import bridgelabz.basicJunitTest.Calculator;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class CalculatorTest {
+    Calculator calc = new Calculator(20,10);
+
+    @Test
+    public void testAddition() {
+        assertEquals(30,calc.add(calc.a, calc.b));
+    }
+
+    @Test
+    public void testSubtraction(){
+        assertEquals(calc.subtract(calc.a,calc.b),10);
+    }
+
+    @Test
+    public void testMultiply(){
+        assertEquals(calc.multiply(calc.a,calc.b),200);
+    }
+
+    @Test
+    public void testDivision(){
+        assertThrows(ArithmeticException.class,() -> Calculator.divide(20,0));
+    }
+}
