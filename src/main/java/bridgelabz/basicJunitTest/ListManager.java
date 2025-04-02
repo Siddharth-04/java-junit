0066e04ea6f1c7be3ea33a4e4fae8a22a8143637
Siddharth <siddharth1360.be21@chitkara.edu.in>
2025-04-02 14:42:20 +0530

    [Siddharth] ADD: ListManager class testing

---
+++ src/main/java/bridgelabz/basicJunitTest/ListManager.java
@@ -0,0 +1,19 @@
+package bridgelabz.basicJunitTest;
+
+import java.util.ArrayList;
+
+public class ListManager {
+    public ArrayList<Integer> addElement(ArrayList<Integer> list,int element) {
+        list.add(element);
+        return list;
+    }
+
+    public ArrayList<Integer> removeElement(ArrayList<Integer> list,int element) {
+        list.remove(element);
+        return list;
+    }
+
+    public int getSize(ArrayList<Integer> list) {
+        return list.size();
+    }
+}
---
+++ src/test/java/bridgelabz/ListManagerTest.java
@@ -0,0 +1,46 @@
+package bridgelabz;
+
+import java.util.ArrayList;
+
+import bridgelabz.basicJunitTest.ListManager;
+import org.junit.jupiter.api.*;
+import static org.junit.Assert.*;
+
+public class ListManagerTest {
+    static ListManager listManager;
+    ArrayList<Integer> list =new ArrayList<>();
+
+    @BeforeAll
+    public static void initialize() {
+        listManager = new ListManager();
+    }
+
+    @Test
+    public void addElementTest() {
+        ArrayList<Integer> newArr = new ArrayList<>();
+        newArr.add(1);
+        assertArrayEquals(newArr.toArray(),listManager.addElement(list,1).toArray());
+    }
+
+    @Test
+    public void removeElementTest() {
+        ArrayList<Integer> newArr = new ArrayList<>();
+        newArr.add(1);
+
+        listManager.addElement(list,1);
+        listManager.addElement(list,2);
+
+        assertArrayEquals(newArr.toArray(),listManager.removeElement(list,1).toArray());
+    }
+
+    @Test
+    public void getSizeTest() {
+        listManager.addElement(list,1);
+        listManager.addElement(list,2);
+
+        assertEquals(2,listManager.getSize(list));
+    }
+
+
+
+}
